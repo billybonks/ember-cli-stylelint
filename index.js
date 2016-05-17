ffc3880aaa06c11f45dfc52316c37f60987d4658
billybonks <sebastienstettler@gmail.com>
2016-05-17 16:44:58 +0800

    initial features

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 /* jshint node: true */
 'use strict';
 
+var StyleLinter = require('broccoli-style-lint');
+var Funnel = require('broccoli-Funnel');
+
 module.exports = {
-  name: 'ember-cli-style-lint'
+  name: 'ember-cli-style-lint',
+
+  included: function(app) {
+    if (!app.isTestingStyleLintAddon) {
+      this._super.included(app);
+    }
+
+    this.app = app;
+    this.styleLintOptions = app.options.styleLint || {generateTests:true};
+  },
+
+  lintTree: function(type, tree) {
+    if (type === 'app') {
+      return new Funnel(new StyleLinter(this.app.trees.styles, this.styleLintOptions), {
+        srcDir:'tests',
+      });
+    } else {
+      return tree;
+    }
+  }
 };
