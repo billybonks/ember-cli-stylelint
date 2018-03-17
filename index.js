ddc59668bab05cc3b6959e3ed8ebdbae2eca1335
billybonks <sebastienstettler@gmail.com>
2018-03-17 16:10:13 +0800

    upgrade broccoli-stylelint

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 'use strict';
 
-var mergeTrees = require('broccoli-merge-trees');
-var StyleLinter = require('broccoli-stylelint');
-var Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
+let StyleLinter = require('broccoli-stylelint');
+let Funnel = require('broccoli-funnel');
 const escapeString = require('js-string-escape');
 
 module.exports = {
   name: 'ember-cli-stylelint',
 
-  included: function() {
+  init(){
+    this._super.init && this._super.init.apply(this, arguments);
+
+    let VersionChecker = require('ember-cli-version-checker');
+    let checker = new VersionChecker(this);
+
+    if (checker.for('ember-qunit', 'npm').exists() || checker.for('ember-cli-qunit', 'npm').exists()) {
+      this._testGenerator = 'qunit';
+    } else if (checker.for('ember-mocha', 'npm').exists() || checker.for('ember-cli-mocha', 'npm').exists()) {
+      this._testGenerator = 'mocha';
+    }
+  },
+
+  included() {
     //shared
     //guard see https://github.com/ember-cli/ember-cli/issues/3718
-    var app;
+    let app;
 
     // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
     // use that.
@@ -20,7 +33,7 @@ module.exports = {
     } else {
      // Otherwise, we'll use this implementation borrowed from the _findHost()
      // method in ember-cli.
-     var current = this;
+     let current = this;
      do {
        app = current.app || app;
      } while (current.parent.parent && (current = current.parent));
@@ -41,40 +54,18 @@ module.exports = {
     this.app = app;
   },
 
-  lintTree: function(type, tree) {
-    var project = this.project;
+  lintTree(type) {
 
     if (type === 'app') {
-      this.styleLintOptions.testGenerator =  function(relativePath, errors) {
-        var passed = null;
-        var name = relativePath+' should pass style lint';
-        if (errors) {
-          passed = false;
-          var assertions = [name];
-          for(var i = 0; i < errors.warnings.length; i++){
-            var warning = errors.warnings[i];
-            assertions.push(escapeString('line: '+warning.line+', col: '+warning.column+' '+warning.text+'.'));
-          }
-          errors = assertions.join('\\n');
-        } else {
-          passed = true;
-          errors = "";
-        }
+      this.styleLintOptions.testingFramework = this._testGenerator;
 
-        return project.generateTestFile(' Style Lint ', [{
-          name: name,
-          passed: !!passed,
-          errorMessage: errors
-        }]);
-      };
-
-      var toBeLinted = [ this.app.trees.app ];
+      let toBeLinted = [ this.app.trees.app ];
       if (this.styleLintOptions.includePaths) {
         toBeLinted.push.apply(toBeLinted, this.styleLintOptions.includePaths);
       }
 
-      var linted = toBeLinted.map(function(tree) {
-        var filteredTreeToBeLinted = new Funnel(tree, {
+      let linted = toBeLinted.map(function(tree) {
+        let filteredTreeToBeLinted = new Funnel(tree, {
           exclude: ['**/*.js']
         });
         return StyleLinter.create(filteredTreeToBeLinted, this.styleLintOptions)
